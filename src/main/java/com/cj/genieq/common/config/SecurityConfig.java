2816a0f5e198ec77b1fcfe17b8a81d7f084d747b
hoonee-math <kwanghoon041@gmail.com>
2025-03-07 14:49:31 +0900

    Init: MinHo 님의 DDL 문이용 DB 추가 완료. API 명세서에 명시된 주소값 요청 권한 허용

--- src/main/java/com/cj/genieq/common/config/SecurityConfig.java
+++ src/main/java/com/cj/genieq/common/config/SecurityConfig.java
@@ -21,8 +21,18 @@ public class SecurityConfig {
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 // 요청에 대한 권한 설정
                 .authorizeHttpRequests(auth -> auth
-                                .requestMatchers("/api/test/**").permitAll() // API 요청에 대한 접근 허용
-                                .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
+                        .requestMatchers("/api/test/**").permitAll() // API 요청에 대한 접근 허용
+                        .requestMatchers("/auth/**").permitAll() // 회원 인증
+                        .requestMatchers("/info/**").permitAll() // 회원 정보
+                        .requestMatchers("/tick/**").permitAll() // 이용권
+                        .requestMatchers("/paym/**").permitAll() // 결제
+                        .requestMatchers("/subj/**").permitAll() // 지문주제
+                        .requestMatchers("/pass/**").permitAll() // 지문
+                        .requestMatchers("/favo/**").permitAll() // 즐겨찾기
+                        .requestMatchers("/form/**").permitAll() // 문항형식
+                        .requestMatchers("/ques/**").permitAll() // 문항
+                        .requestMatchers("/noti/**").permitAll() // 공지
+                        .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
                 )
                 .build();
 
