a4479e24f0573cddfff2815d2133664c9265c87a
hoonee-math <kwanghoon041@gmail.com>
2025-03-07 13:26:39 +0900

    Init: CLI 이용 db 데이터 저장 테스트 완료 성공

---
+++ src/main/java/com/cj/genieq/test/controller/TestMemberCommandLineRunner.java
@@ -0,0 +1,37 @@
+package com.cj.genieq.test.controller;
+
+import com.cj.genieq.test.model.dto.TestMember;
+import com.cj.genieq.test.model.service.TestMemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Scanner;
+
+@Component
+@RequiredArgsConstructor
+class TestMemberCommandLineRunner implements CommandLineRunner {
+
+    private final TestMemberService service;
+
+    @Override
+    public void run(String... args) {
+        Scanner sc = new Scanner(System.in);
+        String name = "";
+
+        while(!name.equals("q")) {
+            System.out.print("DB에 저장시킬 이름을 입력해주세요 (종료: q): ");
+            name = sc.next();
+            sc.nextLine(); // 버퍼 비우기
+
+            if(!name.equals("q")) {
+                TestMember member = TestMember.builder().name(name).build();
+                TestMember savedMember = service.saveMember(member);
+                System.out.println(name + " 저장 완료! 생성된 멤버 코드: " + savedMember.getMemberCode());
+            }
+        }
+        sc.close();
+        System.out.println("프로그램 종료");
+        System.exit(0);
+    }
+}
