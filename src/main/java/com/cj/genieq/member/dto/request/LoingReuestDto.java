4cdf0616e8e1b061e888cbfc94250edf4847d2d5
yejin <hgkim12271@gmail.com>
2025-03-09 18:40:35 +0900

    Feat : 즐겨찾기 추가/해제

--- src/main/java/com/cj/genieq/member/dto/request/LoingReuestDto.java
+++ src/main/java/com/cj/genieq/member/dto/request/LoingReuestDto.java
@@ -3,6 +3,7 @@ package com.cj.genieq.member.dto.request;
 import lombok.*;
 import org.hibernate.annotations.IdGeneratorType;
 
+//단순한 로그인 데이터를 전달하기 때문에 @data를 안 쓰고, @getter/@setter를 쓴 거다.
 @Getter
 @Setter
 public class LoingReuestDto {
