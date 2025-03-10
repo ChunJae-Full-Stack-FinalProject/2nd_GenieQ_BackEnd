207631781995a75cde2f241cef7ccb62b613080c
daineee424 <daineee424@gmail.com>
2025-03-10 14:49:25 +0900

    Feat: 공지사항 전체 조회

--- src/main/java/com/cj/genieq/notice/service/NoticeService.java
+++ src/main/java/com/cj/genieq/notice/service/NoticeService.java
@@ -1,4 +1,9 @@
 package com.cj.genieq.notice.service;
 
+import com.cj.genieq.notice.dto.response.NoticeListResponseDto;
+
+import java.util.List;
+
 public interface NoticeService {
+    List<NoticeListResponseDto> getNoticeList(String type, int page, int size);
 }
--- src/main/java/com/cj/genieq/notice/service/NoticeServiceImpl.java
+++ src/main/java/com/cj/genieq/notice/service/NoticeServiceImpl.java
@@ -1,4 +1,36 @@
 package com.cj.genieq.notice.service;
 
-public class NoticeServiceImpl {
+import com.cj.genieq.notice.dto.response.NoticeListResponseDto;
+import com.cj.genieq.notice.entity.NoticeEntity;
+import com.cj.genieq.notice.repository.NoticeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class NoticeServiceImpl implements NoticeService {
+
+    private final NoticeRepository noticeRepository;
+
+    @Override
+    public List<NoticeListResponseDto> getNoticeList(String type, int page, int size) {
+
+        int startRow = page * size;
+        int endRow = startRow + size;
+
+        List<NoticeEntity> entities = noticeRepository.selectByType(type, startRow, endRow);
+
+        List<NoticeListResponseDto> result = entities.stream()
+                .map(entity->NoticeListResponseDto.builder()
+                        .notCode(entity.getNotCode())
+                        .type(entity.getType())
+                        .title(entity.getTitle())
+                        .date(entity.getDate())
+                        .build())
+                .toList();
+
+        return result;
+    }
 }
