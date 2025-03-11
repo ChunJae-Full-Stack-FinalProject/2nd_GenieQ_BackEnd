c8284485a589a1f6a0121fa6ef10a40941011b3d
daineee424 <daineee424@gmail.com>
2025-03-11 19:49:32 +0900

    Feat: 이용권 결제 기능 구현 수정

--- src/main/java/com/cj/genieq/payment/controller/PaymentController.java
+++ src/main/java/com/cj/genieq/payment/controller/PaymentController.java
@@ -1,6 +1,7 @@
 package com.cj.genieq.payment.controller;
 
 import com.cj.genieq.member.dto.response.LoginMemberResponseDto;
+import com.cj.genieq.payment.dto.request.PaymentRequestDto;
 import com.cj.genieq.payment.service.PaymentService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -15,15 +16,15 @@ public class PaymentController {
     private final PaymentService paymentService;
 
     // 결제 내역 추가
-    @GetMapping("/insert/each")
-    public ResponseEntity<?> insertEach(HttpSession session, @RequestParam(required = false) Long ticCode) {
+    @PostMapping("/insert/each")
+    public ResponseEntity<?> insertEach(HttpSession session, @RequestBody PaymentRequestDto paymentRequestDto) {
         LoginMemberResponseDto loginMember = (LoginMemberResponseDto) session.getAttribute("LOGIN_USER");
 
         if (loginMember == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인이 필요합니다.");
         }
 
-        paymentService.insertPayment(loginMember.getMemberCode(), ticCode);
+        paymentService.insertPayment(loginMember.getMemberCode(), paymentRequestDto.getTicCode());
 
         return ResponseEntity.ok().body("결제 성공");
     }
